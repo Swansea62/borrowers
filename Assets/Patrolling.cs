dd2824c8715c257a5066421d179271d190676814
Ben Swan <swanben223@gmail.com>
2022-03-25 10:13:36 +0000

    Added Patrol Script

---
+++ Assets/Patrolling.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrolling : MonoBehaviour
+{
+    public Transform[] points;
+    int current;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != points[current].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, points[current].position, speed * Time.deltaTime);
+        }
+        else
+        {
+            current = (current + 1) % points.Length;
+        }
+
+        if (transform.position == points[current].position)
+        {
+            transform.Rotate(0.0f, 90.0f, 0.0f);
+        }
+    }
+}

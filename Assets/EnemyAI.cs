121daaceb26348a4261fc3da47c2d8e6f53a661f
Ben Swan <swanben223@gmail.com>
2022-03-30 12:00:18 +0100

    EnemyAI modified

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public AiSensor sensor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sensor = GetComponent<AiSensor>();
     }
 
     // Update is called once per frame
